2eeec47d87c0b5774ed192a37b5bcd6168447273
Danny <daniel-acosta710@live.com>
2016-11-22 22:58:20 -0500

    Voice recognition!!!!!!!!!!!!!!!!

---
+++ OverlookWeb/speech2txt.js
@@ -0,0 +1,89 @@
+﻿// startup code
+var text = 'Haga su consulta ahora';
+var response;
+
+// ask the problem
+ask(text, function (err, result) {
+    if (err) {
+        document.getElementById('result').innerHTML = 'No se registró su entrada de voz.';
+    } else {
+        var answer = result.transcript;
+        answer = answer.toLowerCase();
+        var heroes = ['genji', 'mccree', 'pharah', 'reaper', 'soldado 76', 'sombra', 'tracer', 'bastión', 'hanzo', 'junkrat', 'mei', 'torbjorn', 'widowmaker', 'dva', 'reinhardt', 'roadhog', 'winston', 'zarya', 'ana', 'lucio', 'mercy', 'symmetra', 'zenyatta'];
+        var listo = include(heroes, answer);
+        if (listo!=-1) {
+            response = 'Quieres un counter de: '+listo;
+            speak(response);
+            document.getElementById('result').innerHTML = response;
+            window.location.href = "/ver_heroe.aspx?id=" + (includeid(heroes,answer)+1);
+        } else {
+            response = 'Una disculpa, no entendí '+answer+'.';
+            speak(response);
+            document.getElementById('result').innerHTML = response;
+        }
+    }
+})
+function include(arr, obj) {
+    for(var i=0; i<arr.length; i++) {
+        if (arr[i].includes(obj)) return arr[i];
+    }
+    return -1;
+}
+function includeid(arr, obj) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].includes(obj)) return i;
+    }
+    return -1;
+}
+// ask a question and get an answer
+function ask(text, callback) {
+    // ask question
+    speak(text, function () {
+        // get answer
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = false;
+        recognition.interimResults = false;
+
+        recognition.onend = function (e) {
+            if (callback) {
+                callback('no results');
+            }
+        };
+
+        recognition.onresult = function (e) {
+            // cancel onend handler
+            recognition.onend = null;
+            if (callback) {
+                callback(null, {
+                    transcript: e.results[0][0].transcript,
+                    confidence: e.results[0][0].confidence
+                });
+            }
+        }
+
+        // start listening
+        recognition.start();
+    });
+}
+
+
+// say a message
+function speak(text, callback) {
+    var u = new SpeechSynthesisUtterance();
+    u.text = text;
+    u.lang = 'en-US';
+
+    u.onend = function () {
+        if (callback) {
+            callback();
+        }
+    };
+
+    u.onerror = function (e) {
+        if (callback) {
+            callback(e);
+        }
+    };
+
+    speechSynthesis.speak(u);
+}
---
+++ OverlookWeb/voz.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Overlook.Master" AutoEventWireup="true" CodeBehind="voz.aspx.cs" Inherits="OverlookWeb.voz" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        #cph div{
+            text-align:center;
+            font-family:BigNoodle;
+            margin:0 auto;
+            margin-top:1em;
+            font-size:10em;
+        }
+        output{
+            text-align:center;
+            font-size:1em;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <output id="result"></output>
+    <output id="resultao"></output>
+    <div><a href="voz.aspx">Pregunta de nuevo</a></div>
+    <script src="speech2txt.js"></script>
+</asp:Content>
